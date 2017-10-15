7a95f803b1d1bd12087749dfe46f31cc0cfccd87
KristynaCerna <kristtynacerna@gmail.com>
2017-10-15 19:51:02 +0200

    Mars
    Backward direction fixed and switch for movement instructions added

--- src/main/java/dk/cngroup/university/Mars.java
+++ src/main/java/dk/cngroup/university/Mars.java
@@ -5,19 +5,16 @@ public class Mars {
     private Landscape landscape;
     private RoverPosition position;
 
+
     public Mars(Rover rover, Landscape landscape, RoverPosition position) {
         this.rover = rover;
         this.landscape = landscape;
         this.position = position;
     }
 
-    public RoverPosition getPosition() {
-        return position;
-    }
-
     public RoverPosition moveForward() {
-        RoverPosition forwardPosition = RoverPositionFactory.getForwardPosition(position, rover.getDirection());
-
+        RoverPosition forwardPosition = RoverPositionFactory
+                .getForwardPosition(position, rover.getDirection());
         if (landscape.isFieldAccessible(forwardPosition)) {
             return forwardPosition;
         } else {
@@ -25,13 +22,35 @@ public class Mars {
         }
     }
 
-    public RoverPosition moveBackward(){
-        RoverPosition backwardPosition = RoverPositionFactory.getBackwardPosition(position, rover.getDirection());
-
+    public RoverPosition moveBackward() {
+        RoverPosition backwardPosition = RoverPositionFactory
+                .getBackwardPosition(position, rover.getDirection());
         if (landscape.isFieldAccessible(backwardPosition)) {
             return backwardPosition;
-        }else {
-            return position; // send location, take picture and collect sample
+        } else {
+            return position;
         }
+    }
+
+    public RoverPosition getPosition() {
+        return position;
+    }
+
+    public void movementInstructions(Mars mars, Rover rover, Instruction instruction) {
+        switch (instruction) {
+            case FORWARD:
+                mars.moveForward();
+                break;
+            case BACKWARD:
+                mars.moveBackward();
+                break;
+            case TURNLEFT:
+                rover.turnLeft();
+                break;
+            case TURNRIGHT:
+                rover.turnRight();
+                break;
+        }
+        throw new RuntimeException("this command is unknown");
     }
 }
